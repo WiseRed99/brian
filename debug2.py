82d99383da75d4f779266d83a2cbaf039068559a
brian <bri4n.huang@gmail.com>
2023-05-31 11:51:40 -0700

    corrected debug2

--- debug2.py
+++ debug2.py
@@ -1,13 +1,13 @@
 def score_to_letter_grade(grade):
-    if grade > 90:
+    if grade >= 90:
         return "A"
     elif grade >= 87:
         return "B+"
-    elif grade == 80:
+    elif grade >= 80:
         return "B"
     elif grade >= 77:
         return "C+"
-    elif grade <= 70:
+    elif grade >= 70:
         return "C"
     elif grade >= 67:
         return "D+"
@@ -15,6 +15,7 @@ def score_to_letter_grade(grade):
         return "D"
     else:
         return "F"
+
 
 print("Grade of 90 should be A: " + score_to_letter_grade(90))
 print("Grade of 87 should be B+: " + score_to_letter_grade(87))
@@ -24,3 +25,4 @@ print("Grade of 70 should be C: " + score_to_letter_grade(70))
 print("Grade of 67 should be D+: " + score_to_letter_grade(67))
 print("Grade of 60 should be D: " + score_to_letter_grade(60))
 print("Grade of 59 should be F: " + score_to_letter_grade(59))
+
